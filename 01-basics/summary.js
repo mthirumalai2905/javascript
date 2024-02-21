08ca9f86134fa54e021df54a359605e5fdd473c9
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-21 21:49:07 +0530

    added summary

---
+++ 01-basics/summary.js
@@ -0,0 +1,12 @@
+const heros = ["luffy", "zoro", "sanji"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+        console.log("Hello world");
+}
+
+console.log(typeof myFunction)
