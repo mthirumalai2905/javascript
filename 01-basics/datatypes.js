9bcaf3bd1bff74900a4bc07806fbbba709572195
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-21 19:29:09 +0530

    added datatypes.js

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert() // we are using nodejs and not browser 
+
+console.log(3+3+3);
+console.log("thiru");
+
+let name = "luffy";
+let age = 18;
+let sex;
+let isLoggedIn = false;
+console.log(name);
+console.log(age);
+console.log(isLoggedIn);
+
+
+//number
+//string
+//boolean
+//null => standalone value representational empty value
+//undefined => 
+//symbol => uniqueness
+
+
+// object 
+console.log(typeof age);
+console.log(typeof sex);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
