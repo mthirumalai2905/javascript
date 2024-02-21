582e838a4b08cbfd9e3bcfff5e9bbac427e610de
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-21 19:44:36 +0530

    added conversions.js

---
+++ 01-basics/conversion.js
@@ -0,0 +1,21 @@
+let score = undefined;
+console.log(typeof score);
+
+let scory = String (score);
+console.log(typeof scory);
+console.log(scory);
+
+let name = "Thiru";
+
+console.log(typeof name); //string
+
+let namy = Number (name);// type casting
+console.log(typeof name);//string
+console.log(typeof namy);// number
+console.log(namy);// NaN
+
+let someNumber = 69;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
