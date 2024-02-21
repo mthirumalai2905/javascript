8f98f11e1d5bd531b7f5c718e475f4f6948157fc
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-21 20:13:54 +0530

    added operations.js

---
+++ 01-basics/operations.js
@@ -0,0 +1,36 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 * 200);
+console.log(2 ** 3);
+console.log(2%3);
+
+let str1 = "hello";
+let str2 = "luffy";
+
+console.log(str1 + " " + str2);//hello luffy
+console.log(1 + "2");//12
+console.log("1" + 2);//12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); //32
+
+console.log(+true); //1
+console.log(true); //true
+
+console.log(false); //false
+console.log(+false); //0
+
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+console.log(num1);
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
