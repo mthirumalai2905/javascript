355c6bf5a051e1647b42b3b378247038b98bc6a2
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-22 12:33:54 +0530

    added nu,ber.js

--- beginner/number.js
+++ beginner/number.js
@@ -1,5 +1,34 @@
-const score = 69;
+// // const score = 69;
 
-const balance = new Number(100);
-console.log(typeof balance);
-console.log(balance);
+// // const balance = new Number(100);
+// // console.log(typeof balance);
+// // console.log(balance);
+
+// // const otherNumber = 123.8966;
+
+// // console.log(otherNumber.toPrecision(3));
+
+// ///////////////////////////Maths/////////////////////
+
+// // console.log('====================================');
+// // console.log(Math.PI);
+// // console.log('====================================');
+
+// // console.log('====================================');
+// // console.log(Math.abs(-4));
+// // console.log('====================================');
+
+// // console.log('====================================');
+// // console.log(Math.ceil(4.3));
+// // console.log('====================================');
+
+// // console.log('====================================');
+// // console.log(Math.floor(1.5));
+// // console.log('====================================');
+
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
