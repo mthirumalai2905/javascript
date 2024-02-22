0c816ea36214f99bb33c7d918a8664fa08274fa0
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-22 12:52:25 +0530

    1

---
+++ beginner/datetime.js
@@ -0,0 +1,17 @@
+// // // let myDate =  new Date();
+// // // console.log(myDate.toString());
+// // // console.log(myDate.toLocaleDateString());
+// // // console.log(myDate.toJSON());
+// // // console.log(typeof myDate);
+
+// // let myTimeStamp = Date.now()
+
+// // console.log(myTimeStamp.toPrecision(2));
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date();
+// console.log(newDate.getDay());
+
+// newDate.toLocaleString('')
