ce6f1a7fede77c87c6b5d98db3ed2a8abdca7908
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-03-09 14:14:42 +0530

    added express

---
+++ MERN/express/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req,res) => {
+    return res.send("Hello from home page")
+})
+
+app.get('/about', (req,res)=>{
+    return res.send("Hello from about page");
+})
+
+
+app.listen(4000, ()=>console.log("Server started"));
